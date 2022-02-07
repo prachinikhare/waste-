76966cd5cb5bdaaa2a38795c73e5e6a3f1546859
Prachi Nikhare <82951655+prachinikhare@users.noreply.github.com>
2022-02-07 19:38:08 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,61 +1,35 @@
-from flask import Flask, request, render_template, redirect, jsonify, url_for
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from flask import Flask, request, render_template, redirect, jsonify
 from flask_jsglue import JSGlue # this is use for url_for() working inside javascript which is help us to navigate the url
 import util
 import os
 from werkzeug.utils import secure_filename
+from flask_cors import cross_origin
+import pandas as pd
+import numpy as np
 
 application = Flask(__name__, template_folder='template')
-application.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///feedback.db"
-application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(application)
-
-class FEEDBACKPAGE(db.Model):
-    sno = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(200), nullable=False)
-    email = db.Column(db.String(500), nullable=False)
-    message = db.Column(db.String(500), nullable=False)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
 # JSGlue is use for url_for() working inside javascript which is help us to navigate the url
 jsglue = JSGlue() # create a object of JsGlue
 jsglue.init_app(application) # and assign the app as a init app to the instance of JsGlue
+
 util.load_artifacts()
 
 #home page
-@application.route("/")
-def index():
-    feedbacksection = FEEDBACKPAGE.query.order_by(FEEDBACKPAGE.date_created.desc()).all()
-    return render_template('index.html', feedbacksection=feedbacksection)
+@application.route("/",methods=['GET'])
+@cross_origin()
+def home():
+    return render_template("index.html")
 
-@application.route('/feedbacks/<int:feedbacks_id>')
-def feedbacks(feedbacks_id):
-    feedbacks = FEEDBACK.query.filter_by(id=feedbacks_id).one()
-    return render_template('feedback.html', feedbacks=feedbacks)
-    
-@application.route("/about")
+@application.route("/",methods=['GET'])
+@cross_origin()
 def about():
     return render_template("about.html")
 
-@application.route('/feedback')
-def feedback():
-    return render_template('feedback.html')
-
-@application.route('/Feedback', methods=['POST'])
-def Feedback():
-    name = request.form['name']
-    email = request.form['email']
-    message  = request.form['message']
-    feedbacksection = FEEDBACK(name=name, email=email, message = message, date_created=datetime.now())
-    db.session.feedback(feedbacksection)
-    db.session.commit()
-    return redirect(url_for('index'))
-
-
 #classify waste
-@application.route("/classify", methods = ["GET","POST"])
-def classify():
+@application.route("/classifywaste", methods = ["POST"],,methods=['GET', 'POST'])
+@cross_origin()
+def classifywaste():
     image_data = request.files["file"]
     #save the image to upload
     basepath = os.path.dirname(__file__)
@@ -66,11 +40,17 @@ def classify():
     os.remove(image_path)
     return jsonify(predicted_value=predicted_value, details=details, video1=video1, video2=video2)
 
+@application.route("/",methods=['GET'])
+@cross_origin()
+def feedback():
+    return render_template("feedback.html")
+    
 # here is route of 404 means page not found error
 @application.errorhandler(404)
+@cross_origin()
 def page_not_found(e):
     # here i created my own 404 page which will be redirect when 404 error occured in this web app
     return render_template("404.html")
-    
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     application.run(debug=True)
