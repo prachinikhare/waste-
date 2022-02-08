877f0cf9b3690b8386dff4fab4b5532783c6b37c
Prachi Nikhare <82951655+prachinikhare@users.noreply.github.com>
2022-02-08 22:28:56 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template, redirect, jsonify
 
-application = Flask(__name__,template_folder='template', static_folder='static')
+application = Flask(__name__,template_folder='templates', static_folder='static')
 
 #home page
 @application.route("/index")
