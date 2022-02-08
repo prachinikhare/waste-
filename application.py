98838c31e9fcf5d633130e72ec19065b819b5b39
Prachi Nikhare <82951655+prachinikhare@users.noreply.github.com>
2022-02-08 23:28:33 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,17 +1,4 @@
 from flask import Flask, request, render_template, redirect, jsonify
-
-application = Flask(__name__,template_folder='templates', static_folder='static')
-
-#home page
-@application.route("/index")
-@application.route("/")
-def index():
-    return render_template("index.html")
-
-if __name__ == '__main__':
-    application.debug = True
-    application.run()
-from flask import Flask, request, render_template, redirect, jsonify
 from flask_jsglue import JSGlue # this is use for url_for() working inside javascript which is help us to navigate the url
 import util
 import os
