f2a9afed7ec16539fcb1f77670dc6f9a1b0d37b8
Prachi Nikhare <82951655+prachinikhare@users.noreply.github.com>
2022-02-08 20:42:33 +0530

    Update application.py

--- application.py
+++ application.py
@@ -8,12 +8,6 @@ application = Flask(__name__,template_folder='template', static_folder='static')
 def index():
     return render_template("index.html")
 
-# here is route of 404 means page not found error
-@application.errorhandler(404)
-def page_not_found(e):
-    # here i created my own 404 page which will be redirect when 404 error occured in this web app
-    return render_template("404.html")
-
 if __name__ == '__main__':
     application.debug = True
     application.run()
