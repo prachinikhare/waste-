81f02b8f29de0f9256907c9ca147550c30819e43
Prachi Nikhare <82951655+prachinikhare@users.noreply.github.com>
2022-02-17 19:54:27 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,9 +1,22 @@
-from flask import Flask, request, render_template, redirect, jsonify
+from flask import Flask, request, render_template, redirect, jsonify, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 from flask_jsglue import JSGlue # this is use for url_for() working inside javascript which is help us to navigate the url
 import util
 import os
 from werkzeug.utils import secure_filename
+
 application = Flask(__name__ , template_folder='templates',instance_relative_config=True, static_url_path = "/static", static_folder = "static")
+application.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///feedback.db"
+application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(application)
+
+class FEEDBACKPAGE(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(500), nullable=False)
+    message = db.Column(db.String(500), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
 # JSGlue is use for url_for() working inside javascript which is help us to navigate the url
 jsglue = JSGlue() # create a object of JsGlue
@@ -21,10 +34,30 @@ def home():
 def about():
     return render_template("about.html")
 
+@application.route("/")
+def index():
+    feedbacksection = FEEDBACKPAGE.query.order_by(FEEDBACKPAGE.date_created.desc()).all()
+    return render_template('about.html', feedbacksection=feedbacksection)
+
+@application.route('/feedbacks/<int:feedbacks_id>')
+def feedbacks(feedbacks_id):
+    feedbacks = FEEDBACK.query.filter_by(id=feedbacks_id).one()
+    return render_template('feedback.html', feedbacks=feedbacks)
+
 @application.route('/feedback')
 @application.route("/feedback.html")
 def feedback():
     return render_template("feedback.html")
+
+@application.route('/Feedback', methods=['POST'])
+def Feedback():
+    name = request.form['name']
+    email = request.form['email']
+    message  = request.form['message']
+    feedbacksection = FEEDBACK(name=name, email=email, message = message, date_created=datetime.now())
+    db.session.feedback(feedbacksection)
+    db.session.commit()
+    return redirect(url_for('about'))
 
 #classify waste
 @application.route('/')
